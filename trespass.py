60f2a3f86cf169532db1215a32ef9d28af166391
Graham Smith <gps1539@gmail.com>
2017-01-06 11:50:22 -0800

    initial code

---
+++ trespass.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+import sys
+import os
+import re
+import getopt
+import gpgme
+import io
+import numpy as np
+import argparse
+
+# initialize gpg
+c = gpgme.Context()
+
+# set directory to user's home (should work for Linux, Mac and Windows)
+os.chdir(os.path.expanduser("~"))
+if os.path.exists('.trespass')==False:
+	os.mkdir('.trespass')
+os.chdir(os.path.expanduser('.trespass'))
+
+# command line options and help
+parser = argparse.ArgumentParser()
+parser.add_argument("--init", nargs='+', help="initialize with gpg key")
+parser.add_argument("--add", nargs='+', help="add an account, username and password")
+parser.add_argument("--remove", help="remove an account")
+parser.add_argument("--accounts", help="list all accounts",action="store_true")
+parser.add_argument("--showuser", help="show the username for an account")
+parser.add_argument("--showpass", nargs='+', help="show the password for a user/account")
+
+#parser.add_argument("--portfolio", help="choose a portfolio")
+args = parser.parse_args()
+
+if args.add:
+	if len(args.add)!=3:
+		print('account, username and password are required with --add')
+		sys.exit(1)
+
+if args.showpass:
+	if len(args.showpass)!=2:
+		print('account and username are required with --showpass')
+		sys.exit(1)
+
+if args.init:
+	if len(args.init)!=2:
+		print('please provide two gpg keys with --init')
+		sys.exit(1)
+
+if args.init:
+	k1=str(args.init[0])
+	key1={1:k1}
+	np.save('key1.npy', key1)
+	print(key1[1])
+	k2=str(args.init[1])
+	key2={1:k2}
+	np.save('key2.npy', key2)
+	print(key2[1])
+	sys.exit(1)
+
+if os.path.exists('key1.npy')==False:
+	print('Please initialize with --init <gpg-key>')
+else:
+	keys1 = np.load('key1.npy').item()
+	recipient= c.get_key(str(keys1[1]))
+
+if os.path.exists('key2.npy')==False:
+	print('Please initialize with --init <gpg-key>')
+else:
+	keys2 = np.load('key2.npy').item()
+	recipient2= c.get_key(str(keys2[1]))
+
+if os.path.exists('acc.npy.gpg')==False:
+	acc={}
+	np.save('acc.npy', acc)
+else:
+	with open('acc.npy.gpg', 'rb') as input_file:
+		with open('acc.npy', 'wb') as output_file:
+			c.decrypt(input_file, output_file)
+acc=np.load('acc.npy').item()
+os.remove('acc.npy')
+
+if os.path.exists('user.npy.gpg')==False:
+	user={}
+	np.save('user.npy', acc)
+else:
+	with open('user.npy.gpg', 'rb') as input_file:
+		with open('user.npy', 'wb') as output_file:
+			c.decrypt(input_file, output_file)
+user=np.load('user.npy').item()
+os.remove('user.npy')
+
+# function to add username and password for an account
+def inputtoacc(account, username, password):
+	acc.update({account:username})
+	np.save('acc.npy', acc)
+	with open('acc.npy', 'rb') as input_file:
+		with open('acc.npy.gpg', 'wb') as output_file:
+			c.encrypt([recipient], 0, input_file, output_file)	
+	accuser=((str(account)) + (str(username)))
+	print(accuser)
+	user.update({accuser:password})
+	np.save('user.npy', user)
+	with open('user.npy', 'rb') as input_file:
+		with open('user.npy.gpg', 'wb') as output_file:
+			c.encrypt([recipient2], 0, input_file, output_file)
+	os.remove('acc.npy')
+	os.remove('user.npy')
+
+# function to remove an account     
+def removeacc(account):
+	if account in acc:
+		accuser=((str(account)) + (acc[(account)]))
+		del acc[account]
+		np.save('acc.npy', acc)
+		with open('acc.npy', 'rb') as input_file:
+			with open('acc.npy.gpg', 'wb') as output_file:
+				c.encrypt([recipient], 0, input_file, output_file)		
+	else:
+		return
+	if accuser in user:
+		del user[accuser]
+		np.save('user.npy', user)
+		with open('user.npy', 'rb') as input_file:
+			with open('user.npy.gpg', 'wb') as output_file:
+				c.encrypt([recipient2], 0, input_file, output_file)
+	os.remove('acc.npy')
+	os.remove('user.npy')
+	return
+
+# call inputtoacc if --add option on command line
+if args.add:
+	account=str(args.add[0])
+	username=str(args.add[1])
+	password=str(args.add[2])
+	inputtoacc(account,username,password)
+
+# call removeacc if --delete option on command line      
+if args.remove:
+	account=str(args.remove)
+	removeacc(account)
+	sys.exit(1)
+
+# if --accounts option on command line      
+if args.accounts:
+	for key in acc.keys():
+		print(key)
+	sys.exit(1)
+
+# if --showuser option on command line      
+if args.showuser:
+	account=str(args.showuser)
+	if account in acc:	
+		print(acc[account])
+	else:
+		print('Error no account "' + (account) + '"')	
+		sys.exit(1)
+
+# call --showpass option on command line
+if args.showpass:
+	account=str(args.showpass[0])
+	username=str(args.showpass[1])
+	if account in acc:
+		accuser=((str(account)) + (acc[(account)]))
+		print(user[accuser])
+
+# polite exit if no acc in dictionary and --added not used
+if len(acc)==0:
+	print('Please input account with --add')
+	sys.exit(1)
+
